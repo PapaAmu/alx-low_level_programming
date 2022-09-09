c8c874539ed0d1711525a515b455d42a3c8aeb56
Themba Real Lukhele <realthemba@gmail.com>
2022-09-08 19:06:39 -0700

    still working on the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
-
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle);
-   return (0);
+	char stringChar[] = "\"Programming is like building a multilingual puzzle";
+	puts(stringChar);
+	return (0);
 }
