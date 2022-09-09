37e973d3ea9b75a1b4a2454d9fce023eae56b0d6
Themba Real Lukhele <realthemba@gmail.com>
2022-09-08 19:13:25 -0700

    printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char Grammar[60] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", Grammar);
+
+	return (0);
+
+}
