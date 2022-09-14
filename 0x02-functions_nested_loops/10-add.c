8baccce7880fc899bb09bcc19d3aea647f931baa
Themba Real Lukhele <realthemba@gmail.com>
2022-09-14 09:51:05 -0700

    addng two int values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer to be added
+ * @b: The 2nd integer to be added
+ *
+ * Return: The sum of the two added numbers
+ */
+
+in add(int a, int b)
+{
+	return (a + b);
+}
