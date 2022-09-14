64d3b0e7b776ad088ce22c475b3304a9128eafe0
Themba Real Lukhele <realthemba@gmail.com>
2022-09-13 19:09:13 -0700

    check if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Description: checks if a character is lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true and 0 if false
+ */
+int _islower(int check_)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (check_ == i)
+		{
+			return(1);
+		}
+	}
+	return (0);
+}
