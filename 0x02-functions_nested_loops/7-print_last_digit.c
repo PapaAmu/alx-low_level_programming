450e8f14f13a28444b16ee23544b630a544e36d4
Themba Real Lukhele <realthemba@gmail.com>
2022-09-14 06:31:26 -0700

    fixing compile error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#iinclude "main.h"
 
 /**
  * print_last_digit - prints the last digit of a number
@@ -7,20 +7,14 @@
  * Return: integer
  */
 
-int print_last_digits(int n)
+int print_last_digit(int n)
 {
-	int digit_a;
-	
+	int last = n % 10;
+
 	if (n < 0)
-		n = -n;
-
-	digit_a = n % 10;
-
-	if (digit_a < 0)
-		digit_a = -digit_a;
-
-	_putchar(digit_a + '0');
-
-
-	return (digit_a);
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
