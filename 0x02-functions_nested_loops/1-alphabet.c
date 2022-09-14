921c3177ae94ca45afe753008841fc43dbfa44fa
Themba Real Lukhele <realthemba@gmail.com>
2022-09-13 18:48:59 -0700

    printing alaphabet in lowercases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#inclide "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a function that prints alphabet,
+ * in lowercase followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
