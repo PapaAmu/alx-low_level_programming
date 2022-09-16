a5bc4c9b67340733b72cf86b0d51523f2e9de642
Themba Real Lukhele <realthemba@gmail.com>
2022-09-15 19:11:20 -0700

    multiply two values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - gives the product of a and b
+ * @value1: variable
+ * @value2: variable
+ *
+ * Return: value1 * value2
+ */
+
+int mul(int value1, int value2)
+{
+return (value1 * value2);
+}
